77c94a7c8b92ffbdb4066c7c0b3e11d9ee270c03
monkenWu <610877102@mail.nknu.edu.tw>
2020-04-18 12:45:52 +0800

    同步官方使用文件

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,11 +1,10 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.0.0-rc.4',
-    LANGUAGE: 'None',
+    VERSION: '4.0.2',
+    LANGUAGE: 'zh_TW',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
 };
